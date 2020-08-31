50c524f825bb2fc6f4b625c58e0c3a9214179313
dahobt123 <66279896+dahobt123@users.noreply.github.com>
2020-08-31 08:22:30 -0500

    Update objects to reflect costing and crews microservices

--- src/main/java/com/charterandgo/model/Aircraft.java
+++ src/main/java/com/charterandgo/model/Aircraft.java
@@ -26,16 +26,16 @@ public class Aircraft implements Serializable {
 
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        builder.append(nnumber + "-");
-        builder.append(ageofaircraft + "-");
-        builder.append(manufacturer + "-");
-        builder.append(model + "-");
-        builder.append(enginetype + "-");
-        builder.append(Integer.toString(numberofengines) + "-");
-        builder.append(galley + "-");
-        builder.append(head + "-");
-        builder.append(Integer.toString(numberofpax) + "-");
-        for (RichMedia media : richmedia) {
+        builder.append(getNnumber() + "-");
+        builder.append(getAgeofaircraft() + "-");
+        builder.append(getManufacturer() + "-");
+        builder.append(getModel() + "-");
+        builder.append(getEnginetype() + "-");
+        builder.append(Integer.toString(getNumberofengines()) + "-");
+        builder.append(getGalley() + "-");
+        builder.append(getHead() + "-");
+        builder.append(Integer.toString(getNumberofpax()) + "-");
+        for (RichMedia media : getRichmedia()) {
             builder.append(media.toString() + "-");
         }
         return builder.toString();
@@ -44,5 +44,85 @@ public class Aircraft implements Serializable {
     public JSONObject toJson() {
         JSONObject obj = new JSONObject();
         return obj;
+    }
+
+    public String getNnumber() {
+        return nnumber;
+    }
+
+    public void setNnumber(String nnumber) {
+        this.nnumber = nnumber;
+    }
+
+    public String getAgeofaircraft() {
+        return ageofaircraft;
+    }
+
+    public void setAgeofaircraft(String ageofaircraft) {
+        this.ageofaircraft = ageofaircraft;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getEnginetype() {
+        return enginetype;
+    }
+
+    public void setEnginetype(String enginetype) {
+        this.enginetype = enginetype;
+    }
+
+    public int getNumberofengines() {
+        return numberofengines;
+    }
+
+    public void setNumberofengines(int numberofengines) {
+        this.numberofengines = numberofengines;
+    }
+
+    public String getGalley() {
+        return galley;
+    }
+
+    public void setGalley(String galley) {
+        this.galley = galley;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public int getNumberofpax() {
+        return numberofpax;
+    }
+
+    public void setNumberofpax(int numberofpax) {
+        this.numberofpax = numberofpax;
+    }
+
+    public List<RichMedia> getRichmedia() {
+        return richmedia;
+    }
+
+    public void setRichmedia(List<RichMedia> richmedia) {
+        this.richmedia = richmedia;
     }
 }
